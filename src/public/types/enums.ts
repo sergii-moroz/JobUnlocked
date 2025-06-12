6885f215e7182a23f10cec35279b6c80c437c270
oliferovych <dimasikolif@gmail.com>
2025-06-12 22:32:47 +0200

    add/change types for frontend

---
+++ src/public/types/enums.ts
@@ -0,0 +1,6 @@
+export enum JobType {
+	FullTime = 1,
+	PartTime = 2,
+	Internship = 3,
+	WorkingStudent = 4,
+}
--- src/public/types/job-offer.ts
+++ src/public/types/job-offer.ts
@@ -1,10 +1,12 @@
+import { JobType } from "./enums.js";
+
 export interface JobOffer {
 	id: string;
 	createdBy: string;
 	title: string;
 	description: string;
 	requirements: string;
-	type: number;
+	type: JobType;
 	jobStatus: number;
 	location: string;
 	salary: number;
